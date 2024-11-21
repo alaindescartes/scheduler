d12bcff042873e4132be12f6b2473ee0cbf31f67
alain descartes <ualaindescartes@gmail.com>
2024-11-21 00:21:41 -0700

    corrected root route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import MainOutlet from "./_components/outlets/MainOutlet.jsx";
-import RootOutlet from "./_components/outlets/RootOutlet.jsx";
 import Login from "./_components/pages/Login.jsx";
 import MainPage from "./_components/pages/MainPage.jsx";
 import Register from "./_components/pages/Register.jsx";
@@ -8,39 +11,33 @@ import Register from "./_components/pages/Register.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootOutlet />,
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    // Login page
+    path: "login",
+    element: <Login />,
+  },
+  {
+    // Register page
+    path: "sign-up",
+    element: <Register />,
+  },
+  {
+    // Main page
+    path: "homepage",
+    element: <MainOutlet />,
     children: [
       {
-        //login page
-        path: "login",
+        element: <MainPage />,
         index: true,
-        element: <Login />,
-      },
-      {
-        //register page
-        path: "sign-up",
-        element: <Register />,
-      },
-      {
-        // main page
-        path: "homepage",
-        element: <MainOutlet />,
-        children: [
-          {
-            element: <MainPage />,
-            index: true,
-          },
-        ],
       },
     ],
   },
 ]);
+
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
