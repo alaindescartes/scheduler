38beb1b6d63b4949e478d14af10b22b26fb3c4c8
alain descartes <ualaindescartes@gmail.com>
2024-12-05 00:08:01 -0700

    added add residence btn

--- frontend/src/_components/pages/Residences.jsx
+++ frontend/src/_components/pages/Residences.jsx
@@ -3,7 +3,10 @@ import { Button } from "../../components/ui/button.jsx"
 import Residence from "../Residence.jsx"
 function Residences() {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 ">
+      <Button className="bg-blue-400 w-[300px] text-center m-auto mt-3 hover:bg-blue-500">
+        ADD RESIDENCES
+      </Button>
       {/* list of residences */}
       <div>
         <Residence />
