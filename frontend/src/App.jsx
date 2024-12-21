061e68dcfca416cf3583f17d03ead50b26011c59
alain descartes <ualaindescartes@gmail.com>
2024-12-21 15:11:02 -0700

    added residence overview route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,34 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
-import MainOutlet from "./_components/outlets/MainOutlet.jsx"
-import Login from "./_components/pages/Login.jsx"
-import MainPage from "./_components/pages/MainPage.jsx"
-import Register from "./_components/pages/Register.jsx"
-import ProtectedRoutes from "./_components/outlets/ProtectedRoutes.jsx"
-import Residences from "./_components/pages/Residences.jsx"
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+import MainOutlet from './_components/outlets/MainOutlet.jsx';
+import Login from './_components/pages/Login.jsx';
+import MainPage from './_components/pages/MainPage.jsx';
+import Register from './_components/pages/Register.jsx';
+import ProtectedRoutes from './_components/outlets/ProtectedRoutes.jsx';
+import Residences from './_components/pages/Residences.jsx';
+import ResidentOverview from '@/_components/pages/ResidenceOverview.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Navigate to="/login" replace />,
   },
   {
     // Login page
-    path: "login",
+    path: 'login',
     element: <Login />,
   },
   {
     // Register page
-    path: "sign-up",
+    path: 'sign-up',
     element: <Register />,
   },
   {
     // Main page
-    path: "homepage",
+    path: 'homepage',
     element: <MainOutlet />,
     children: [
       {
@@ -42,14 +47,22 @@ const router = createBrowserRouter([
             <Residences />
           </ProtectedRoutes>
         ),
-        path: "residence",
+        path: 'residence',
+      },
+      {
+        element: (
+          <ProtectedRoutes>
+            <ResidentOverview />
+          </ProtectedRoutes>
+        ),
+        path: 'visit_residence/:id',
       },
     ],
   },
-])
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
