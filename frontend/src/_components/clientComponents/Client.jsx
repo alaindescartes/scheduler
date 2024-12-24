bce707668b7451b8972629ee4acb0abf9cc501a2
Alain Descartes <ualaindescartes@gmail.com>
2024-12-23 22:54:31 -0700

    removed edit btn

--- frontend/src/_components/clientComponents/Client.jsx
+++ frontend/src/_components/clientComponents/Client.jsx
@@ -79,9 +79,6 @@ function Client({ loading, client, residenceId }) {
         {/* Admin-only buttons */}
         {userRole === 'admin' && (
           <>
-            <Button className="bg-blue-500 text-white px-4 py-2 w-full md:w-auto rounded-md hover:bg-blue-600 transition-all">
-              EDIT INFO
-            </Button>
             <Button className="bg-red-500 text-white px-4 py-2 w-full md:w-auto rounded-md hover:bg-red-600 transition-all">
               DELETE CLIENT
             </Button>
